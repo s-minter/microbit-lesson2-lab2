d7465b789648d44eae651b1ab3fd0cf34ed9f8dc
s-minter <70456772+s-minter@users.noreply.github.com>
2020-08-30 14:44:37 -0700

    Changed from square LEDS to diagonal triangle

--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
-
+function drawTriangle () {
+    for (let column = 0; column <= 4; column++) {
+        for (let row = 0; row <= column; row++) {
+            led.plot(column, row)
+        }
+    }
+}
+function drawDiagonal2 () {
+    for (let index = 0; index <= 4; index++) {
+        led.plot(4 - index, index)
+    }
+}
+function drawRow (row: number) {
+    for (let column = 0; column <= 4; column++) {
+        led.plot(column, row)
+    }
+}
+function drawSquare () {
+    for (let column = 0; column <= 4; column++) {
+        for (let row = 0; row <= 4; row++) {
+            led.plot(column, row)
+        }
+    }
+}
+function drawDiagonal () {
+    for (let index = 0; index <= 4; index++) {
+        led.plot(index, index)
+    }
+}
+drawTriangle()
